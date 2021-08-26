9f2b26f9bb41c4bbf2f8181cc07273b745039842
MartaAzaanoune <marta.azaanoune2016@gmail.com>
2021-08-26 16:51:09 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/" >Start</Link>
+        </li>
+        <li>
+          <Link to="/o_co_chodzi" >O co chodzi?</Link>
+        </li>
+        <li>
+          <Link to="/o_nas" >O nas</Link>
+        </li>
+        <li>
+          <Link to="/fundacje_i_organizacje" >Fundacja i organizacje</Link>
+        </li>
+        <li>
+          <Link to="/kontakt" >Kontakt</Link>
+        </li>
+        <li>
+          <Link to="/zaloguj">Zaloguj</Link>
+        </li>
+        <li>
+          <Link to="/zal_konto">Załóż konto</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/o_co_chodzi">
+          <O_co_chodzi/>
+        </Route>
+        <Route path="/o_nas">
+          <O_nas/>
+        </Route>
+        <Route path="/fundacje_i_organizacje">
+          <Fundacje_i_organizacje/>
+        </Route>
+        <Route path="/kontakt">
+          <Kontakt/>
+        </Route>
+        <Route path="/zaloguj">
+          <Zaloguj/>
+        </Route>
+        <Route path="/zal_konto">
+          <Konto/>
+        </Route>
+
+
+      </Switch>
+
+    </Router>
   );
 }
 
